647b1749f0de9613c390c1774ee47a361842a839
eslaila <148451285+eslaila@users.noreply.github.com>
2023-12-04 18:41:46 +0100

    app2.py

--- dashboard/app2.py
+++ dashboard/app2.py
@@ -1,19 +1,6 @@
-from branca.colormap import LinearColormap
 import streamlit as st
 import geopandas as gpd
-import folium
-from streamlit_folium import folium_static
-import numpy as np
-import rasterio 
-import glob
 import matplotlib.pyplot as plt
-from PIL import Image, ImageDraw
-st.set_page_config(
-    page_title="TIMELAPSE",
-    page_icon="⏳",
-    layout="centered",  
-)
-
 import pandas as pd
 import contextily as ctx
 import numpy as np
@@ -23,9 +10,13 @@ import os
 from matplotlib.animation import FuncAnimation
 import rasterio as rio
 from pyproj import Transformer 
+import folium
+from branca.colormap import LinearColormap
+from streamlit_folium import folium_static
+import glob
 from PIL import Image, ImageDraw, ImageFont
 import imageio
-
+from branca.colormap import LinearColormap
 
 st.set_page_config(
     page_title="TIMELAPSE",
@@ -71,7 +62,7 @@ def create_timelapse(image_files, DAY_names, duration):
 
 
 DAY_names = ['0', '1', '2', '3', '4', '5', '6']
-folder = "dashboard/RASTERSclassifié"
+folder = "RASTERSclassifié"
 if selected_attribute=='temperature':
       min=0
       max=100
@@ -117,7 +108,7 @@ st.write('voici en format VIDEO 🎥 ')
 video_frames = []
 for selected_day in range(0, 6):  # Include day 1 in the range
         # Charger le raster correspondant à l'attribut sélectionné
-        RASTERPATH = f"dashboard/RASTERSclassifié"
+        RASTERPATH = f"RASTERSclassifié"
         
         ## LC08 RGB Image
         dst_crs = 'EPSG:4326'
@@ -169,7 +160,7 @@ for selected_day in range(0, 6):  # Include day 1 in the range
         # Overlay raster (RGB) called img using add_child() function (opacity and bounding box set)
         m.add_child(folium.raster_layers.ImageOverlay(img.transpose(1, 2, 0), opacity=.7, 
                         bounds=bounds_fin))
-        path = f"dashboard/RASTERSclassifié\\{selected_attribute}jour{selected_day}.tif"
+        path = f"RASTERSclassifié\\{selected_attribute}jour{selected_day}.tif"
 
         with rio.open(path) as src:
             data = src.read(1, masked=True)
@@ -208,4 +199,3 @@ if video_frames:
     # Remove the video file after displaying
     if os.path.exists(video_path):
         os.remove(video_path)
-
